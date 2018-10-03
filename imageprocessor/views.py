cdb9f5687be103904f52f859747548fc8f0b4ce8
Jarret <jarret.a.miller@gmail.com>
2018-10-02 21:12:52 -0500

    add: photo tagging functionality

--- imageprocessor/views.py
+++ imageprocessor/views.py
@@ -1,13 +1,140 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from imageprocessor.forms import ImageForm
+import six.moves.urllib as urllib
+import tensorflow as tf
+from PIL import Image
+import numpy as np
+from imageprocessor.detection_class_map import detection_class_map
+from pathlib import Path
+import os
+import tarfile
 
 # Create your views here.
 def index(request):
-	context = {}
-	if request.method =='POST':
-		context['form'] = 'hey you uploaded an image'
-		return render(request, 'index.html', context)
-	form = ImageForm(request.POST or None)
-	context['form'] = form
-	return render(request, 'index.html', context)
+    context = {}
+    if request.method =='POST':
+        MODEL_NAME = 'ssd_mobilenet_v1_coco_2017_11_17'
+        MODEL_FILE = MODEL_NAME + '.tar.gz'
+        DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
+        PATH_TO_FROZEN_GRAPH = MODEL_NAME + '/frozen_inference_graph.pb'
+        PATH_TO_LABELS = MODEL_NAME + '/mscoco_label_map.pbtxt'
+        frozen_graph = Path(PATH_TO_FROZEN_GRAPH)
+        if not frozen_graph.is_file():
+            opener = urllib.request.URLopener()
+            opener.retrieve(DOWNLOAD_BASE + MODEL_FILE, MODEL_FILE)
+            tar_file = tarfile.open(MODEL_FILE)
+            for file in tar_file.getmembers():
+                file_name = os.path.basename(file.name)
+                if 'frozen_inference_graph.pb' in file_name:
+                    tar_file.extract(file, os.getcwd())
+        image = Image.open(request.FILES.get('file'))
+        image_np = load_image_into_numpy_array(image)
+        detection_graph = tf.Graph()
+        with detection_graph.as_default():
+            od_graph_def = tf.GraphDef()
+            with tf.gfile.GFile(PATH_TO_FROZEN_GRAPH, 'rb') as fid:
+                serialized_graph = fid.read()
+                od_graph_def.ParseFromString(serialized_graph)
+                tf.import_graph_def(od_graph_def, name='')
+        output_dict = run_inference_for_single_image(image_np, detection_graph)
+        context['form'] = detection_class_map[output_dict['detection_classes'][0]]
+        return render(request, 'index.html', context)
+    form = ImageForm(request.POST or None)
+    context['form'] = form
+    return render(request, 'index.html', context)
+
+def run_inference_for_single_image(image, graph):
+    with graph.as_default():
+        with tf.Session() as sess:
+          # Get handles to input and output tensors
+          ops = tf.get_default_graph().get_operations()
+          all_tensor_names = {output.name for op in ops for output in op.outputs}
+          tensor_dict = {}
+          for key in [
+              'num_detections', 'detection_boxes', 'detection_scores',
+              'detection_classes', 'detection_masks'
+          ]:
+            tensor_name = key + ':0'
+            if tensor_name in all_tensor_names:
+              tensor_dict[key] = tf.get_default_graph().get_tensor_by_name(
+                  tensor_name)
+          if 'detection_masks' in tensor_dict:
+            # The following processing is only for single image
+            detection_boxes = tf.squeeze(tensor_dict['detection_boxes'], [0])
+            detection_masks = tf.squeeze(tensor_dict['detection_masks'], [0])
+            # Reframe is required to translate mask from box coordinates to image coordinates and fit the image size.
+            real_num_detection = tf.cast(tensor_dict['num_detections'][0], tf.int32)
+            detection_boxes = tf.slice(detection_boxes, [0, 0], [real_num_detection, -1])
+            detection_masks = tf.slice(detection_masks, [0, 0, 0], [real_num_detection, -1, -1])
+            detection_masks_reframed = reframe_box_masks_to_image_masks(
+                detection_masks, detection_boxes, image.shape[0], image.shape[1])
+            detection_masks_reframed = tf.cast(
+                tf.greater(detection_masks_reframed, 0.5), tf.uint8)
+            # Follow the convention by adding back the batch dimension
+            tensor_dict['detection_masks'] = tf.expand_dims(
+                detection_masks_reframed, 0)
+          image_tensor = tf.get_default_graph().get_tensor_by_name('image_tensor:0')
+
+          # Run inference
+          output_dict = sess.run(tensor_dict,
+                                 feed_dict={image_tensor: np.expand_dims(image, 0)})
+
+          # all outputs are float32 numpy arrays, so convert types as appropriate
+          output_dict['num_detections'] = int(output_dict['num_detections'][0])
+          output_dict['detection_classes'] = output_dict[
+              'detection_classes'][0].astype(np.uint8)
+          output_dict['detection_boxes'] = output_dict['detection_boxes'][0]
+          output_dict['detection_scores'] = output_dict['detection_scores'][0]
+          if 'detection_masks' in output_dict:
+              output_dict['detection_masks'] = output_dict['detection_masks'][0]
+    return output_dict
+
+def reframe_box_masks_to_image_masks(box_masks, boxes, image_height,
+                                     image_width):
+  """Transforms the box masks back to full image masks.
+  Embeds masks in bounding boxes of larger masks whose shapes correspond to
+  image shape.
+  Args:
+    box_masks: A tf.float32 tensor of size [num_masks, mask_height, mask_width].
+    boxes: A tf.float32 tensor of size [num_masks, 4] containing the box
+           corners. Row i contains [ymin, xmin, ymax, xmax] of the box
+           corresponding to mask i. Note that the box corners are in
+           normalized coordinates.
+    image_height: Image height. The output mask will have the same height as
+                  the image height.
+    image_width: Image width. The output mask will have the same width as the
+                 image width.
+  Returns:
+    A tf.float32 tensor of size [num_masks, image_height, image_width].
+  """
+  # TODO(rathodv): Make this a public function.
+  def reframe_box_masks_to_image_masks_default():
+    """The default function when there are more than 0 box masks."""
+    def transform_boxes_relative_to_boxes(boxes, reference_boxes):
+      boxes = tf.reshape(boxes, [-1, 2, 2])
+      min_corner = tf.expand_dims(reference_boxes[:, 0:2], 1)
+      max_corner = tf.expand_dims(reference_boxes[:, 2:4], 1)
+      transformed_boxes = (boxes - min_corner) / (max_corner - min_corner)
+      return tf.reshape(transformed_boxes, [-1, 4])
+
+    box_masks_expanded = tf.expand_dims(box_masks, axis=3)
+    num_boxes = tf.shape(box_masks_expanded)[0]
+    unit_boxes = tf.concat(
+        [tf.zeros([num_boxes, 2]), tf.ones([num_boxes, 2])], axis=1)
+    reverse_boxes = transform_boxes_relative_to_boxes(unit_boxes, boxes)
+    return tf.image.crop_and_resize(
+        image=box_masks_expanded,
+        boxes=reverse_boxes,
+        box_ind=tf.range(num_boxes),
+        crop_size=[image_height, image_width],
+        extrapolation_value=0.0)
+  image_masks = tf.cond(
+      tf.shape(box_masks)[0] > 0,
+      reframe_box_masks_to_image_masks_default,
+      lambda: tf.zeros([0, image_height, image_width, 1], dtype=tf.float32))
+  return tf.squeeze(image_masks, axis=3)
+
+def load_image_into_numpy_array(image):
+  (im_width, im_height) = image.size
+  return np.array(image.getdata()).reshape(
+      (im_height, im_width, 3)).astype(np.uint8)
