19051dc2ea3ed2c2a755b51dd4a1827a1daa84de
Jarret <jarret.a.miller@gmail.com>
2018-10-14 16:49:36 -0500

    added: integrated tagservice into django view. Changed url so tagger is at /classify

--- CatalogueRaisonne/urls.py
+++ CatalogueRaisonne/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from imageprocessor.views import index
+from imageprocessor.views import classify, index
 
 urlpatterns = [
+    path('', index, name='index'),
     path('admin/', admin.site.urls),
-    path('', index, name='index')
+    path('classify/', classify, name='classify')
 ]
